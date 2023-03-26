1177ae5f9d6f5ef091f5fb58ca0f5fa5b305c99c
Gabriel de Oliveira <Gabriel-Dev-Oliveira>
2023-03-26 17:29:29 -0300

    primeiro commit

---
+++ login_laravel/public/index.php
@@ -0,0 +1,154 @@
+<?php
+// Inicialize a sessão
+session_start();
+ 
+// Verifique se o usuário já está logado, em caso afirmativo, redirecione-o para a página de boas-vindas
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+    header("location: welcome.php");
+    exit;
+}
+ 
+// Incluir arquivo de configuração
+require_once "config.php";
+ 
+// Defina variáveis e inicialize com valores vazios
+$username = $password = "";
+$username_err = $password_err = $login_err = "";
+ 
+// Processando dados do formulário quando o formulário é enviado
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+    // Verifique se o nome de usuário está vazio
+    if(empty(trim($_POST["username"]))){
+        $username_err = "Por favor, insira o nome de usuário.";
+    } else{
+        $username = trim($_POST["username"]);
+    }
+    
+    // Verifique se a senha está vazia
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Por favor, insira sua senha.";
+    } else{
+        $password = trim($_POST["password"]);
+    }
+    
+    // Validar credenciais
+    if(empty($username_err) && empty($password_err)){
+        // Prepare uma declaração selecionada
+        $sql = "SELECT id, username, password FROM users WHERE username = :username";
+        
+        if($stmt = $pdo->prepare($sql)){
+            // Vincule as variáveis à instrução preparada como parâmetros
+            $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
+            
+            // Definir parâmetros
+            $param_username = trim($_POST["username"]);
+            
+            // Tente executar a declaração preparada
+            if($stmt->execute()){
+                // Verifique se o nome de usuário existe, se sim, verifique a senha
+                if($stmt->rowCount() == 1){
+                    if($row = $stmt->fetch()){
+                        $id = $row["id"];
+                        $username = $row["username"];
+                        $hashed_password = $row["password"];
+                        if(password_verify($password, $hashed_password)){
+                            // A senha está correta, então inicie uma nova sessão
+                            session_start();
+                            
+                            // Armazene dados em variáveis de sessão
+                            $_SESSION["loggedin"] = true;
+                            $_SESSION["id"] = $id;
+                            $_SESSION["username"] = $username;                            
+                            
+                            // Redirecionar o usuário para a página de boas-vindas
+                            header("location: welcome.php");
+                        } else{
+                            // A senha não é válida, exibe uma mensagem de erro genérica
+                            $login_err = "Nome de usuário ou senha inválidos.";
+                        }
+                    }
+                } else{
+                    // O nome de usuário não existe, exibe uma mensagem de erro genérica
+                    $login_err = "Nome de usuário ou senha inválidos.";
+                }
+            } else{
+                echo "Ops! Algo deu errado. Por favor, tente novamente mais tarde.";
+            }
+
+            // Fechar declaração
+            unset($stmt);
+        }
+    }
+    
+    // Fechar conexão
+    unset($pdo);
+}
+?>
+ 
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<style>
+    body{
+    background-image:url('https://inspirationhut.net/wp-content/uploads/2013/05/201.png');
+    }
+</style>
+<body>
+<section id="header">
+        <a href="#"><img src="logo3.png" class="logo" alt="logo"></a>
+
+        <div>
+            <ul id="navbar">
+                <li><a href="index.html">Home</a></li>
+                <li><a href="Doações.html">Doações</a></li>
+                <li><a href="Blog.html">Blog</a></li>
+                <li><a href="Sobre_nos.html">Sobre nós</a></li>
+                <li><a class="active" href="Contato.html">Contato</a></li>
+                <li><a href="Sacola.html"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-square" viewBox="0 0 16 16">
+  <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+  <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm12 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1v-1c0-1-1-4-6-4s-6 3-6 4v1a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12z"/>
+</svg></a></li>
+            </ul>
+        </div>
+        <div id="mobile">
+            <a href="Sacola.html"><i class="far fa-shopping-bag"></i></a>
+            <i id="bar" class="fas fa-outdent"></i>
+        </div>
+</section>
+    <div class="wrapper">
+        <h2>Login</h2>
+        <p>Por favor, preencha os campos para fazer o login.</p>
+
+        <?php 
+        if(!empty($login_err)){
+            echo '<div class="alert alert-danger">' . $login_err . '</div>';
+        }        
+        ?>
+
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div class="form-group">
+                <label>Nome do usuário</label>
+                <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
+                <span class="invalid-feedback"><?php echo $username_err; ?></span>
+            </div>    
+            <div class="form-group">
+                <label>Senha</label>
+                <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>">
+                <span class="invalid-feedback"><?php echo $password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <div class="botao_registo">
+                    <input type="submit" value="Entrar">
+                </div>    
+            </div>
+            <p>Não tem uma conta? <a href="register.php">Inscreva-se agora</a>.</p>
+        </form>
+    </div>
+</body>
+</html>
